91ede775f787bda104009def4d4884e4206ef4ea
Arthur H <rah9eu@virginia.edu>
2021-05-14 19:36:48 -0400

    Replaced Our Image Classification Script
    Replaced the old image classification script with the updated, fully (hypothetically) working version.

--- cloud-project-testing/CS6111AWSScript.sh
+++ cloud-project-testing/CS6111AWSScript.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-
 wirelessCard='FillInWithActualNetworkAdapterHere'
+#NEED TO GET THE NETWORK CARD FROM SERVER
 echo 'Input CNN model to train. Use other script for NLP'
 read model
-
-echo 'The model to be trained is $model'
+#Turn the model into the correct form for the script
+modelScript = "native:$model:32:299"
+echo "The model to be trained is $model"
 
 echo 'Feed in a file of hosts/workers'
 #Here, include localhost plus any other workers to be used
@@ -17,42 +18,101 @@ echo 'Number of GPUs to use?'
 read numberOfGPUs
 
 throughputFile="${model}_Throughput_${numberOfGPUs}GPU_${numberOfWorkers}Workers"
-echo 'For $numberOfWorkers worker with $numberofGPUs GPU(s)'
+echo "For $numberOfWorkers worker with $numberofGPUs GPU(s)"
+
+
 echo 'Running with 10 Gbps'
-./wondershaper -a $wirelessCard -d 10000000 -u 10000000
+
+while read ipAddr; do
+  ssh ubuntu@$ipAddr ./wondershaper -a $wirelessCard -d 10000000 -u 10000000
+done <$workerFile
+
+python3 incubator-mxnet/example/image-classification/benchmark.py --worker_file $workerFile --worker_count $numberOfWorkers --gpu_count $numberOfGPUs --networks modelScript >> $throughputFile
+echo -e '\n'>> $throughputFile
+
 
 echo 'Running with 9 Gbps'
 
+while read ipAddr; do
+  ssh ubuntu@$ipAddr ./wondershaper -a $wirelessCard -d 9000000 -u 9000000
+done <$workerFile
+
+python3 incubator-mxnet/example/image-classification/benchmark.py --worker_file $workerFile --worker_count $numberOfWorkers --gpu_count $numberOfGPUs --networks modelScript >> $throughputFile
+echo -e '\n'>> $throughputFile
 
 
 echo 'Running with 8 Gbps'
 
 while read ipAddr; do
-  ssh -i /path/my-key-pair.pem my-instance-user-name@$ipAddr ./wondershaper -a $wirelessCard -d 8000000 -u 8000000
+  ssh ubuntu@$ipAddr ./wondershaper -a $wirelessCard -d 8000000 -u 8000000
 done <$workerFile
 
-#SSH in to each other machine in workerFile and rate limit
-#Unsure what the hell to put here, however to run the benchmark then need to play with model name. Alternatively, hard code for 3 models
-python benchmark.py --worker_file $workerFile --worker_count $numberOfWorkers --gpu_count 1 --networks 'native:inception-v3:32:299' > $throughputFile
-echo -e '\n'> $throughputFile
+python3 incubator-mxnet/example/image-classification/benchmark.py --worker_file $workerFile --worker_count $numberOfWorkers --gpu_count $numberOfGPUs --networks modelScript >> $throughputFile
+echo -e '\n'>> $throughputFile
 
 echo 'Running with 7 Gbps'
-./wondershaper -a $wirelessCard -d 7000000 -u 7000000
+
+while read ipAddr; do
+  ssh ubuntu@$ipAddr ./wondershaper -a $wirelessCard -d 7000000 -u 7000000
+done <$workerFile
+
+python3 incubator-mxnet/example/image-classification/benchmark.py --worker_file $workerFile --worker_count $numberOfWorkers --gpu_count $numberOfGPUs --networks modelScript >> $throughputFile
+echo -e '\n'>> $throughputFile
 
 echo 'Running with 6 Gbps'
-./wondershaper -a $wirelessCard -d 6000000 -u 6000000
+
+while read ipAddr; do
+  ssh ubuntu@$ipAddr ./wondershaper -a $wirelessCard -d 6000000 -u 6000000
+done <$workerFile
+
+python3 incubator-mxnet/example/image-classification/benchmark.py --worker_file $workerFile --worker_count $numberOfWorkers --gpu_count $numberOfGPUs --networks modelScript >> $throughputFile
+echo -e '\n'>> $throughputFile
 
 echo 'Running with 5 Gbps'
-./wondershaper -a $wirelessCard -d 5000000 -u 5000000
+
+while read ipAddr; do
+  ssh ubuntu@$ipAddr ./wondershaper -a $wirelessCard -d 5000000 -u 5000000
+done <$workerFile
+
+python3 incubator-mxnet/example/image-classification/benchmark.py --worker_file $workerFile --worker_count $numberOfWorkers --gpu_count $numberOfGPUs --networks modelScript >> $throughputFile
+echo -e '\n'>> $throughputFile
 
 echo 'Running with 4 Gbps'
-./wondershaper -a $wirelessCard -d 4000000 -u 4000000
+
+while read ipAddr; do
+  ssh ubuntu@$ipAddr ./wondershaper -a $wirelessCard -d 4000000 -u 4000000
+done <$workerFile
+
+python3 incubator-mxnet/example/image-classification/benchmark.py --worker_file $workerFile --worker_count $numberOfWorkers --gpu_count $numberOfGPUs --networks modelScript >> $throughputFile
+echo -e '\n'>> $throughputFile
 
 echo 'Running with 3 Gbps'
-./wondershaper -a $wirelessCard -d 3000000 -u 3000000
+
+while read ipAddr; do
+  ssh ubuntu@$ipAddr ./wondershaper -a $wirelessCard -d 3000000 -u 3000000
+done <$workerFile
+
+python3 incubator-mxnet/example/image-classification/benchmark.py --worker_file $workerFile --worker_count $numberOfWorkers --gpu_count $numberOfGPUs --networks modelScript >> $throughputFile
+echo -e '\n'>> $throughputFile
 
 echo 'Running with 2 Gbps'
-./wondershaper -a $wirelessCard -d 2000000 -u 2000000
+
+while read ipAddr; do
+  ssh ubuntu@$ipAddr ./wondershaper -a $wirelessCard -d 2000000 -u 2000000
+done <$workerFile
+
+python3 incubator-mxnet/example/image-classification/benchmark.py --worker_file $workerFile --worker_count $numberOfWorkers --gpu_count $numberOfGPUs --networks modelScript >> $throughputFile
+echo -e '\n'>> $throughputFile
 
 echo 'Running with 1 Gbps'
-./wondershaper -a $wirelessCard -d 1000000 -u 1000000
+
+while read ipAddr; do
+  ssh ubuntu@$ipAddr ./wondershaper -a $wirelessCard -d 1000000 -u 1000000
+done <$workerFile
+
+python3 incubator-mxnet/example/image-classification/benchmark.py --worker_file $workerFile --worker_count $numberOfWorkers --gpu_count $numberOfGPUs --networks modelScript >> $throughputFile
+echo -e '\n'>> $throughputFile
+
+./wondershaper -c -a $wirelessCard
+
+echo "Tests complete. Data written to $throughputFile."
